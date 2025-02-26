f92eb069ae9fe33d1655533385c93b2bbd269628
SriMukheshSuru <21ee01025@iitbbs.ac.in>
2025-02-26 16:49:54 +0530

    just first

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".text", {
+    strings: ["ML Engineer", "Data Scientist", "Web Developer", "Competitive Programmer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+});
+ 
